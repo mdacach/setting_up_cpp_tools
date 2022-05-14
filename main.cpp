f522162081c9f410f778ad8c67bca1bf44e9eaf6
Matheus Cardoso <dacachmatheus@gmail.com>
2022-05-14 20:34:38 -0300

    Add cppcheck and clang-tidy

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     std::cout << "Initial code for project\n";
+
+    char a[10];
+    a[10] = 0;
+    int x;
+
+    int b = 2.0;
+    int d = 5;
+
+    std::cout << "hey";
 }
