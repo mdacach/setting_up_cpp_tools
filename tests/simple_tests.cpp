f065ffbc141070f5e5aed6c6b9f7e6ab3260f42e
Matheus Cardoso <dacachmatheus@gmail.com>
2022-05-17 21:50:24 -0300

    Add initial test for Soundex Encoding

--- tests/simple_tests.cpp
+++ tests/simple_tests.cpp
@@ -1,5 +1,23 @@
 #include "catch.hpp"
+#include <string_view>
 
-TEST_CASE() {
-    REQUIRE(2 + 2 == 4);
+class Soundex
+{
+public:
+    auto Encode(std::string_view word) const -> std::string { return std::string{ word }; }
+};
+
+TEST_CASE("Retains sole letter of one-letter word", "[soundex_encoding]")
+{
+    auto soundex = Soundex{};
+
+    auto encoded = soundex.Encode("A");
+
+    CHECK(encoded == "A");
 }
+
+// Test list
+// Manage one letter words
+// Fail when given multiple words as input
+// Assert size is always 4
+// Test 0 padding
