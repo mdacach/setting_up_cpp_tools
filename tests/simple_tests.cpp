d06b8b3ff36b5aa4c7965c50c4914c166ce119b8
Matheus Cardoso <dacachmatheus@gmail.com>
2022-05-17 22:01:49 -0300

    Abstract padding with zeros into private method

--- tests/simple_tests.cpp
+++ tests/simple_tests.cpp
@@ -1,19 +1,32 @@
 #include "catch.hpp"
 #include <string_view>
 
+using namespace std::string_literals;
 class Soundex
 {
 public:
-    auto Encode(std::string_view word) const -> std::string { return std::string{ word }; }
+    auto Encode(std::string_view word) const -> std::string { return PadWithZeros(word); }
+
+private:
+    auto PadWithZeros(std::string_view word) const -> std::string { return std::string{ word } + "000"; }
 };
 
 TEST_CASE("Retains sole letter of one-letter word", "[soundex_encoding]")
 {
     auto soundex = Soundex{};
 
-    auto encoded = soundex.Encode("A");
+    const auto encoded = soundex.Encode("A");
 
-    CHECK(encoded == "A");
+    CHECK(encoded == "A000");
+}
+
+TEST_CASE("Pads zeros until three digits", "[soundex_encoding]")
+{
+    auto soundex = Soundex{};
+
+    const auto encoded = soundex.Encode("I");
+
+    CHECK(encoded == "I000");
 }
 
 // Test list
